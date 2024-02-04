93ce63cbfee23d1dbddb891a43459220208492f1
kanphatratrazer <kanphatratrazer28@gmail.com>
2024-02-04 12:11:11 +0700

    portal page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Logo from './assets/'
+
+import PortalPage from "./components/Portal"
+
+const App = () => {
+  return (
+    <div>
+      <PortalPage/>
+    </div>
+  )
+}
+
+export default App
